6ad3e948733337e42a345e342f15fdeb4bfba278
Simon Parten <quafadas@gmail.com>
2024-05-20 09:41:31 +0200

    .

--- .devcontainer/features/mill/install.sh
+++ .devcontainer/features/mill/install.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Install Mill
-$ curl -L -o /usr/local/bin/mill https://raw.githubusercontent.com/lefou/millw/0.4.11/millw > mill && chmod +x /usr/local/bin/mill
+curl -L -o /usr/local/bin/mill https://raw.githubusercontent.com/lefou/millw/0.4.11/millw && chmod +x /usr/local/bin/mill
 
 # Verify installation
 mill version
