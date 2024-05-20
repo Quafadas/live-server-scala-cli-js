18510025a57a4db29b523f0e763abd96c5e0cd7a
Simon Parten <quafadas@gmail.com>
2024-05-20 10:02:32 +0200

    .

--- .devcontainer/features/mill/install.sh
+++ .devcontainer/features/mill/install.sh
@@ -4,6 +4,6 @@ set -e
 
 # Install Mill
 curl -L -o /usr/local/bin/mill https://raw.githubusercontent.com/lefou/millw/0.4.11/millw && chmod +x /usr/local/bin/mill
+curl -fL "https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz" | gzip -d > /usr/local/bin/cs && chmod +x /usr/local/bin/cs
 
-# Verify installation
-mill version
+cs install metals
