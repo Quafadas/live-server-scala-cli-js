eb1144bebf935a2034cb881493c971ab044e7942
partens <simon.parten@schroders.com>
2024-06-24 15:40:04 +0200

    .

--- project/test/src/liveServer.test.scala
+++ project/test/src/liveServer.test.scala
@@ -140,10 +140,10 @@ trait PlaywrightTest extends CatsEffectSuite:
       IO.sleep(3.seconds) >>
         IO(page.navigate(s"http://localhost:$port")) >>
         IO(assertThat(page.locator("h1")).containsText("HelloWorld", increaseTimeout)) >>
-        // IO.blocking(os.write.over(testDir / "hello.scala", helloWorldCode("Bye"))) >>
-        // IO(assertThat(page.locator("h1")).containsText("ByeWorld", increaseTimeout)) >>
-        // IO.blocking(os.write.append(styleDir(testDir) / "index.less", "h1 { color: red; }")) >>
-        // IO(assertThat(page.locator("h1")).hasCSS("color", "rgb(255, 0, 0)"))
+      // IO.blocking(os.write.over(testDir / "hello.scala", helloWorldCode("Bye"))) >>
+      // IO(assertThat(page.locator("h1")).containsText("ByeWorld", increaseTimeout)) >>
+      // IO.blocking(os.write.append(styleDir(testDir) / "index.less", "h1 { color: red; }")) >>
+      // IO(assertThat(page.locator("h1")).hasCSS("color", "rgb(255, 0, 0)"))
   }
 
   ResourceFunFixture {
