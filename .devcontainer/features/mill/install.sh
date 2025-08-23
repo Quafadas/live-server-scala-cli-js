b04c67946c96ae6278fcc9c45208ecf4bbdbe1de
Simon Parten <quafadas@gmail.com>
2025-08-23 18:00:32 +0200

    .

--- .devcontainer/features/mill/install.sh
+++ .devcontainer/features/mill/install.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Put millw on the path, so we can call 'mill' from the command line.
-curl -L -o /usr/local/bin/mill https://raw.githubusercontent.com/lefou/millw/0.4.11/millw && chmod +x /usr/local/bin/mill
+curl -L -o /usr/local/bin/mill https://repo1.maven.org/maven2/com/lihaoyi/mill-dist/1.0.3/mill-dist-1.0.3-mill.sh && chmod +x /usr/local/bin/mill
 
 # Install Coursier - can call cs from command line
 curl -fL "https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz" | gzip -d > /usr/local/bin/cs && chmod +x /usr/local/bin/cs
